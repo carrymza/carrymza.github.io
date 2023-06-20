18bc1e149bff50823a919c2c0e7dccbbb68a9254
Carlos Ramirez <82486588+carrymza@users.noreply.github.com>
2023-06-20 16:07:11 -0400

    Update dectime.js

--- dectime.js
+++ dectime.js
@@ -3,8 +3,8 @@ function convertDecimalToTime(decimal) {
     var minutes = Math.round((decimal - hours) * 60);
     var suffix = hours >= 12 ? "PM" : "AM";
 
-    if (hours < 0 || hours >= 24) { 
-      alert(hours.toFixed(2) + " is an invalid value for hours. Remember to enter hours between: 0.00 - 23.99" ); 
+    if (isNaN(hours) || hours < 0 || hours >= 24) { 
+      alert("Invalid input for hours. Please enter a valid number between: 0.00 - 23.99" ); 
       return null; // or return a default value, such as "12:00 AM" 
     } else if (hours > 12) { 
         hours = hours - 12; 
